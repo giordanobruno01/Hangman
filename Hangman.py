c740f81bfd644c2e323e0d5869eec13bb0bc1a36
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-06-24 10:13:05 +0100

    adding wordList

--- Hangman.py
+++ Hangman.py
@@ -1,1 +1,11 @@
 class hang:
+    def readFile(self):
+        reader = open("wordList.txt", "r")
+        word = reader.readline()
+        self.wordList = reader.read().split("\n")
+        print(self.wordList)
+    def onePlayer(self):
+        pass  
+
+ob = hang()
+ob.readFile()
