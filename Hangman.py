2ba24927e0978ff457a978d9c6b24a68c2a71a1d
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-06-25 11:09:12 +0100

    new additions to players

--- Hangman.py
+++ Hangman.py
@@ -4,9 +4,9 @@ class hang:
         reader = open("wordList.txt", "r")
         word = reader.readline()
         self.wordList = reader.read().split("\n")
-    def onePlayer(self):
+    def onePlayer(self, name):
         n =  random.randint(0, len(self.wordList)-1)
-        word = self.wordList[n]
+        word = self.wordList[n].strip()
         print("the answer is ", word)
         encrypt = ""
         if(len(word)<=6):
@@ -39,12 +39,14 @@ class hang:
                     encrypt = encrypt+word[i]
                 else:
                     encrypt = encrypt+"_"
-        char = ""            
+        char = ""   
+        points = 10         
         for i in range(10):
             va = encrypt
-            print("\nyou have ",(10-i)," chances" )
+            print("\n",name, " have ",(10-i)," chances" )
             print("your word ", encrypt)
-            char = input("\nguess a letter ")  
+            char = input("\nguess a letter ") 
+            char = char.strip() 
             
             if char in word:
                 encrypt = ""
@@ -54,12 +56,68 @@ class hang:
                         encrypt = encrypt+ char
                     else:
                         encrypt = encrypt+ va[g]
-            if (word==encrypt):
-                print("you completed the game in ", i+1, " chances")
-                print("the answer is ", word)
-                return
+            else:
+                points -=1
 
-        print("you lost, the answer is ", word)    
+            if (word==encrypt):
+                print(name," completed the game in ", i+1, " chances")
+                print("the answer is ", word)
+                print("points earned in this round ", points)
+                return points
+
+        print(name," lost, the answer is ", word)
+        return 0 
+    def multiplePlayer(self, name1, name2):
+
+        turns = int(input("how many turns each player? "))
+        count1= 0
+        count2 = 0
+        for i in range(turns*2):
+            if(i%2==0):
+               count1 += self.onePlayer(name1)
+               print("total score at the moment is ", count1)
+            if(i%2!=0):
+               count2 += self.onePlayer(name2)
+               print("total score at the moment is ", count2)
+
+        if(count1>count2):
+            print(name1," is the winner with ", count1, " points")
+        if(count1<count2):
+            print(name2," is the winner with ", count2, " points")
+        else:
+            print("it is a tie")
 ob = hang()
-ob.readFile()
-ob.onePlayer()
+ob.readFile()            
+print("WELCOME TO MY GAME")
+while(True):
+    print("1 for one player \n2 for two players\n3 to end the game")
+    option = int(input("choose an option "))
+    if(option==1):
+        p = input("player name: ")
+        while(True):
+            ob.onePlayer(p)
+            keep = input("do you want to keep playing? y for YES and n for NO")
+            if(keep =="n"):
+                break
+            
+    elif(option==2):
+        name1 = input("player 1 name ")
+        name2 = input("player 2 name ")
+        while(True):
+            ob.multiplePlayer(name1, name2)
+            keep = input("do you want to keep playing? y for YES and n for NO")
+            if(keep =="n"):
+                break
+    elif(option==3):
+        print("game ended")
+        break
+    else:
+        print("wrong input")           
+
+
+
+
+
+
+p1 = input("player 1 name: ")
+p2 = input("player 2 name: ")
